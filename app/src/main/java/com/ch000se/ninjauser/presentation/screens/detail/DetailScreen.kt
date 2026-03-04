6d909dea654a0528af657a38f90051102e895542
ch000se <svyatikchaykivskiy@gmail.com>
2026-03-04 18:08:10 +0200

    fix: remove vertical scroll

--- app/src/main/java/com/ch000se/ninjauser/presentation/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/ch000se/ninjauser/presentation/screens/detail/DetailScreen.kt
@@ -11,9 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Person
@@ -98,7 +96,6 @@ private fun UserDetails(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .verticalScroll(rememberScrollState())
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
